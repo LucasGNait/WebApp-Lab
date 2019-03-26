dc6faad0c5f4e62830d75279ce890bcdd04c150d
LucasGNait <46492942+LucasGNait@users.noreply.github.com>
2019-03-26 08:53:50 -0600

    Assignments Model done.

---
+++ Star-TedModels/Assignments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarTedModels
+{
+    [Table("Assignments", Schema = "dbo")]
+    public class Assignment
+    {
+        [Key]
+        public int AssignmentID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int OfferingID { get; set; }
+        public short PossibleMarks { get; set; }
+        public short? ExtraBonusMarks { get; set; }
+        public short Weight { get; set; }
+
+    }
+}
