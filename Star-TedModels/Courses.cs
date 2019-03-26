325c712747e53c46d1555cf2fb235d09f1a235c5
LucasGNait <46492942+LucasGNait@users.noreply.github.com>
2019-03-26 09:22:46 -0600

    Finished Courses and CurrentOfferings Model to the best of my knowledge.

--- Star-TedModels/Courses.cs
+++ Star-TedModels/Courses.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Star_TedModels
 {
-    class Program
+    [Table("Courses", Schema = "dbo")]
+    public class Course
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-        }
+        [Key]
+        public string CourseID { get; set; }
+        public string CourseName { get; set; }
+        public decimal Credits { get; set; }
+        public int TotaltHours { get; set; }
+        public int ClassroomType { get; set; }
+        public int Term { get; set; }
+        public decimal Tuition { get; set; }
+        public string Description { get; set; }
     }
 }
--- Star-TedModels/CurrentOfferings.cs
+++ Star-TedModels/CurrentOfferings.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StarTedModels
 {
-    class CurrentOfferings
+    [Table("CurrentOfferings", Schema = "dbo")]
+    public class CurrentOffering
     {
+        [Key]
+        public int OfferingID { get; set; }
+        public string ProgramName { get; set; }
+        public string Semester { get; set; }
+        public string CourseID { get; set; }
     }
 }
